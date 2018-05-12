ea4575aa4856eb57c91e240dce88509ccea404df
SeanSyyue <Sean962081468@gmail.com>
2018-05-12 23:09:12 +0800

    Modify test_video_capture.py

--- snippets/test_video_capture.py
+++ snippets/test_video_capture.py
@@ -1,26 +1,26 @@
 import os
 import cv2
 
-cap = cv2.VideoCapture(os.environ['LIVE_STREAM'])
 
+cap = cv2.VideoCapture(os.environ['LIVE_STREAM'])
 
 # Define the codec and create VideoWriter object
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('data/output.avi', fourcc, 20.0, (1920, 1080))
+out = cv2.VideoWriter('../data/output.avi', fourcc, 20.0, (1920, 1080))
+try:
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret is True:
+            out.write(frame)
 
-# while cap.isOpened():
-for _ in range(100):
-    ret, frame = cap.read()
-    if ret is True:
-        out.write(frame)
-
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cv2.imshow('frame', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        else:
             break
-    else:
-        break
 
-# Release everything if job is finished
-cap.release()
-out.release()
-cv2.destroyAllWindows()
+finally:
+    # Release everything if job is finished
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
